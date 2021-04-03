e3a0a996feb0ed92333f24f4f5f321240688a16a
Roberto Tonino <roberto.tonino5@gmail.com>
2021-04-03 19:38:10 +0200

    feat: added auto registering of api routes

--- server/src/routes/api/register.ts
+++ server/src/routes/api/register.ts
@@ -1,10 +1,41 @@
 import { Application } from 'express'
+import { ApiHandler } from '../../types'
 import getEndpoints from './get'
+import deleteEndpoints from './delete'
+import postEndpoints from './post'
+import patchEndpoints from './patch'
 
 const prependApiPath = (path:string) => `/api${path}`
 
+interface Method {
+	method: string
+	endpoints: ApiHandler[]
+}
+
+const methods: Method[] = [
+  {
+    method: 'get',
+    endpoints: getEndpoints
+  },
+  {
+    method: 'delete',
+    endpoints: deleteEndpoints
+  },
+  {
+    method: 'post',
+    endpoints: postEndpoints
+  },
+  {
+    method: 'patch',
+    endpoints: patchEndpoints
+  }
+]
+
 export function registerApis (app: Application) {
-  getEndpoints.forEach((getApi) => {
-    app.get(prependApiPath(getApi.path), getApi.handler)
+  methods.forEach(({ method, endpoints }) => {
+    endpoints.forEach((endpoint) => {
+    	// @ts-ignore
+      app[method](prependApiPath(endpoint.path), endpoint.handler)
+    })
   })
 }
